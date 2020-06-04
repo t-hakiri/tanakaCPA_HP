8f06c8ae8ac3d232c69c075b281977387ca6b626
takuya hakiri <tyapi4@yahoo.co.jp>
2020-06-04 16:14:45 +0900

    add: second push

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
+console.log('aaaaaaaaaaaaaa')
 
 $(function(){
   $("#toppage").load("toppage.html");
+  $("#footer").load("footer.html");
+  $("#header").load("header.html");
 });
 
-console.log('aaaaaaaaaaaaaa')
+var option = {
+    section : '.js-section',
+    easing: "swing",
+    scrollSpeed: 600,
+    scrollbars: true,
+  };
+
+$(function() {
+	console.log('in')
+	$.scrollify(option);
+	console.log('out')
+});
