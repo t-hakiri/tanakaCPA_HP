e9e8f910bc0a9fe059d194fbf1d6404dff1922c8
yu shimamura <mashiyu0914@gmail.com>
2020-06-05 13:55:34 +0900

    add: headerをつけたためスクロールがずれているのを解消

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
-$(function(){
+$(function () {
   $("#section1").load("section1.html");
   $("#section2").load("section2.html");
   $("#section3").load("section3.html");
-  $("#nav").load("nandemo.html");
+  // $("#nav").load("nandemo.html");
   $("#footer").load("nandemo2.html");
 
-  $.scrollify({section:".box"});
+  $.scrollify({ section: ".box", interstitialSection: "footer" });
 });
-
