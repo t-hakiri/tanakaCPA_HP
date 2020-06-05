f738a3309b7e0071ec1ffc183fea3486d4acde20
takuya hakiri <tyapi4@yahoo.co.jp>
2020-06-05 13:52:32 +0900

    add: ページのコマ割り

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 $(function(){
-  $("#section1").load("section1.html");
-  $("#section2").load("section2.html");
-  $("#section3").load("section3.html");
-  $("#nav").load("nandemo.html");
-  $("#footer").load("nandemo2.html");
+	$("#nav").load("header.html");
+  $("#section1").load("top.html");
+  $("#section2").load("vision.html");
+	$("#section3").load("greeting.html");
+	$("#section4").load("support_for_venture_companies.html");
+	$("#section5").load("business.html");
+	$("#section6").load("company_profile.html");
+  $("#footer").load("footer.html");
 
   $.scrollify({section:".box"});
 });
